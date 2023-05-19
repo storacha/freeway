73f20dc501375a59a9af1eb4979d7f04308654c7
Alan Shaw <alan.shaw@protocol.ai>
2023-05-19 12:22:21 +0100

    feat: support CAR block ordering

--- src/index.js
+++ src/index.js
@@ -55,10 +55,10 @@ async function handler (request, env, ctx) {
   const { searchParams } = ctx
   if (!searchParams) throw new Error('missing URL search params')
 
-  if (searchParams.get('format') === 'raw' || headers.get('Accept') === 'application/vnd.ipld.raw') {
+  if (searchParams.get('format') === 'raw' || headers.get('Accept')?.includes('application/vnd.ipld.raw')) {
     return await handleBlock(request, env, ctx)
   }
-  if (searchParams.get('format') === 'car' || headers.get('Accept') === 'application/vnd.ipld.car') {
+  if (searchParams.get('format') === 'car' || headers.get('Accept')?.includes('application/vnd.ipld.car')) {
     return await handleCar(request, env, ctx)
   }
   return await handleUnixfs(request, env, ctx)
