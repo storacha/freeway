1b1aaec47aef327c1e0343f3064f94ce140bfb3f
Alan Shaw <alan.shaw@protocol.ai>
2023-07-14 12:41:33 +0100

    refactor: do not depend on multiindex

--- src/lib/index/api.ts
+++ src/lib/index/api.ts
@@ -1,8 +1,10 @@
 import { UnknownLink } from 'multiformats/link'
-import { MultiIndexItem } from 'cardex/multi-index/api'
 import { MultihashIndexItem } from 'cardex/multihash-index-sorted/api'
+import { CARLink } from 'cardex/api'
 
-export interface IndexEntry extends MultiIndexItem, MultihashIndexItem {}
+export interface IndexEntry extends MultihashIndexItem {
+  origin: CARLink
+}
 
 export interface Index {
   get (c: UnknownLink): Promise<IndexEntry|undefined>
