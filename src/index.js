0e38b551b708b4d64226ef5e3b6ecfced2e8c961
Petra Jaros <peeja@peeja.com>
2024-10-23 10:05:41 -0400

    Use `@import` and Prettier format in `index.js`

--- src/index.js
+++ src/index.js
@@ -24,15 +24,20 @@ import {
 } from './middleware/index.js'
 
 /**
- * @typedef {import('./bindings.js').Environment} Environment
- * @typedef {import('@web3-storage/gateway-lib').IpfsUrlContext} IpfsUrlContext
- * @typedef {import('@web3-storage/gateway-lib').BlockContext} BlockContext
- * @typedef {import('@web3-storage/gateway-lib').DagContext} DagContext
- * @typedef {import('@web3-storage/gateway-lib').UnixfsContext} UnixfsContext
+ * @import {
+ *   Handler,
+ *   Middleware,
+ *   Context,
+ *   IpfsUrlContext,
+ *   BlockContext,
+ *   DagContext,
+ *   UnixfsContext
+ * } from '@web3-storage/gateway-lib'
+ * @import { Environment } from './bindings.js'
  */
 
 export default {
-  /** @type {import('@web3-storage/gateway-lib').Handler<import('@web3-storage/gateway-lib').Context, import('./bindings.js').Environment>} */
+  /** @type {Handler<Context, Environment>} */
   fetch (request, env, ctx) {
     console.log(request.method, request.url)
     const middleware = composeMiddleware(
@@ -64,14 +69,17 @@ export default {
 }
 
 /**
- * @type {import('@web3-storage/gateway-lib').Middleware<BlockContext & UnixfsContext & IpfsUrlContext, BlockContext & UnixfsContext & IpfsUrlContext, Environment>}
+ * @type {Middleware<BlockContext & UnixfsContext & IpfsUrlContext, BlockContext & UnixfsContext & IpfsUrlContext, Environment>}
  */
 export function withFormatRawHandler (handler) {
   return async (request, env, ctx) => {
     const { headers } = request
     const { searchParams } = ctx
     if (!searchParams) throw new Error('missing URL search params')
-    if (searchParams.get('format') === 'raw' || headers.get('Accept')?.includes('application/vnd.ipld.raw')) {
+    if (
+      searchParams.get('format') === 'raw' ||
+      headers.get('Accept')?.includes('application/vnd.ipld.raw')
+    ) {
       return await handleBlock(request, env, ctx)
     }
     return handler(request, env, ctx) // pass to other handlers
@@ -79,14 +87,17 @@ export function withFormatRawHandler (handler) {
 }
 
 /**
- * @type {import('@web3-storage/gateway-lib').Middleware<DagContext & IpfsUrlContext, DagContext & IpfsUrlContext, Environment>}
+ * @type {Middleware<DagContext & IpfsUrlContext, DagContext & IpfsUrlContext, Environment>}
  */
 export function withFormatCarHandler (handler) {
   return async (request, env, ctx) => {
     const { headers } = request
     const { searchParams } = ctx
     if (!searchParams) throw new Error('missing URL search params')
-    if (searchParams.get('format') === 'car' || headers.get('Accept')?.includes('application/vnd.ipld.car')) {
+    if (
+      searchParams.get('format') === 'car' ||
+      headers.get('Accept')?.includes('application/vnd.ipld.car')
+    ) {
       return await handleCar(request, env, ctx)
     }
     return handler(request, env, ctx) // pass to other handlers
