d67fb4a1e48a1dbb19119736eff57a43489c479b
Felipe Forbeck <felipe.forbeck@gmail.com>
2024-12-09 15:17:23 -0300

    remove resolveDIDKey from ucanto server definition

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,5 @@
 import * as Server from '@ucanto/server'
 import * as CAR from '@ucanto/transport/car'
-import { ok, error } from '@ucanto/core'
-import { DIDResolutionError } from '@ucanto/validator'
 
 /**
  * Creates a UCAN server.
@@ -17,26 +15,6 @@ export function createServer (ctx, service) {
     service,
     catch: err => console.error(err),
     // TODO: wire into revocations
-    validateAuthorization: () => ({ ok: {} }),
-    // @ts-expect-error - The type is not defined in the ucan package, but it supports the method.
-    resolveDIDKey: (did) => resolveDIDKey(did, ctx)
+    validateAuthorization: () => ({ ok: {} })
   })
-}
-/**
- * Resolves the DID key for the given DID.
- *
- * @param {import('@ucanto/interface').DID} did - The DID to resolve
- * @param {import('../middleware/withUcanInvocationHandler.types.js').Context} ctx - The application context
- */
-const resolveDIDKey = (did, ctx) => {
-  if (did) {
-    if (did.startsWith('did:web') && did === ctx.gatewayIdentity.did()) {
-      return ok(ctx.gatewaySigner.toDIDKey())
-    }
-    if (did.startsWith('did:key')) {
-      return ok(did)
-    }
-  }
-
-  return error(new DIDResolutionError(did))
 }
