3d60b6279a6caaa991da230f2f1cf0921534a38e
Felipe Forbeck <felipe.forbeck@gmail.com>
2024-12-17 10:01:54 -0300

    feat(indexer): probabilistic feature flag

--- src/middleware/withLocator.js
+++ src/middleware/withLocator.js
@@ -21,9 +21,7 @@ import { Client } from '@storacha/indexing-service-client'
  */
 export function withLocator (handler) {
   return async (request, env, ctx) => {
-    const useIndexingService = new URL(request.url).searchParams
-      .getAll('ff')
-      .includes('indexing-service')
+    const useIndexingService = isIndexingServiceEnabled(request, env)
 
     const locator = useIndexingService
       ? new IndexingServiceLocator({
@@ -45,4 +43,23 @@ export function withLocator (handler) {
 
     return handler(request, env, { ...ctx, locator })
   }
+}
+
+/**
+ * Determines if the indexing service is enabled. It is enabled if the request
+ * contains the `ff=indexing-service` query parameter or if a random chance
+ * falls within the ramp-up percentage. If `FF_RAMP_UP_PERCENTAGE` is not set,
+ * it defaults to 0%.
+ *
+ * @param {Request} request
+ * @param {LocatorEnvironment} env
+ * @returns {boolean}
+ */
+function isIndexingServiceEnabled (request, env) {
+  const withIndexingServicesArg = new URL(request.url).searchParams
+    .getAll('ff')
+    .includes('indexing-service')
+  const percentage = env.FF_RAMP_UP_PERCENTAGE ? Number(env.FF_RAMP_UP_PERCENTAGE) : 0.0
+  const withIndexerEnabled = Math.random() <= percentage
+  return withIndexingServicesArg || withIndexerEnabled
 }
