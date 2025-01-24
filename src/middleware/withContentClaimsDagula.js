09f749d6327130b701fa9db7b7253c7b83807250
Felipe Forbeck <felipe.forbeck@gmail.com>
2025-01-24 13:09:23 -0300

    address reviewers feedback

--- src/middleware/withContentClaimsDagula.js
+++ src/middleware/withContentClaimsDagula.js
@@ -1,4 +1,5 @@
 import { Dagula } from 'dagula'
+import { base58 } from 'multiformats/bases/base58'
 import * as BatchingFetcher from '@web3-storage/blob-fetcher/fetcher/batching'
 import * as dagPb from '@ipld/dag-pb'
 
@@ -27,24 +28,24 @@ import * as dagPb from '@ipld/dag-pb'
  *   >
  * )}
  */
-export function withContentClaimsDagula (handler) {
+export function withContentClaimsDagula(handler) {
   return async (request, env, ctx) => {
     const { locator } = ctx
     const fetcher = BatchingFetcher.create(locator, ctx.fetch)
     const dagula = new Dagula({
-      async get (cid) {
-        const dagPbContent = await getDagPbContent(env, fetcher, cid)
+      async get(cid) {
+        const dagPbContent = await getDagPbContent(env, fetcher, cid, ctx)
         if (dagPbContent) {
           return dagPbContent
         }
         const res = await fetcher.fetch(cid.multihash)
         return res.ok ? { cid, bytes: await res.ok.bytes() } : undefined
       },
-      async stream (cid, options) {
+      async stream(cid, options) {
         const res = await fetcher.fetch(cid.multihash, options)
         return res.ok ? res.ok.stream() : undefined
       },
-      async stat (cid) {
+      async stat(cid) {
         const res = await locator.locate(cid.multihash)
         return res.ok ? { size: res.ok.site[0].range.length } : undefined
       }
@@ -60,9 +61,10 @@ export function withContentClaimsDagula (handler) {
  * @param {Environment} env
  * @param {import('@web3-storage/blob-fetcher').Fetcher} fetcher
  * @param {import('multiformats').UnknownLink} cid
+ * @param {import('@web3-storage/gateway-lib').Context} ctx
  * @returns {Promise<{ cid: import('multiformats').UnknownLink, bytes: Uint8Array } | undefined>}
  */
-async function getDagPbContent (env, fetcher, cid) {
+async function getDagPbContent(env, fetcher, cid, ctx) {
   if (env.FF_DAGPB_CONTENT_CACHE_ENABLED === 'true' && cid.code === dagPb.code) {
     const cachedBytes = await getCachedDagPbBytes(env, cid)
     if (cachedBytes) {
@@ -72,8 +74,13 @@ async function getDagPbContent (env, fetcher, cid) {
     const res = await fetcher.fetch(cid.multihash)
     if (res.ok) {
       const bytes = await res.ok.bytes()
-      const cachedBytes = await cacheDagPbBytes(env, cid, bytes)
-      return { cid, bytes: cachedBytes }
+      const dagPbNode = dagPb.decode(bytes)
+      if (dagPbNode.Links && dagPbNode.Links.length === 0) {
+        // Old DAG PB nodes have no links ("raw" blocks as leaves), so we don't want to cache them
+        return { cid, bytes }
+      }
+      ctx.waitUntil(cacheDagPbBytes(env, cid, bytes))
+      return { cid, bytes }
     }
   }
   return undefined
@@ -86,23 +93,19 @@ async function getDagPbContent (env, fetcher, cid) {
  * @param {Environment} env
  * @param {import('multiformats').UnknownLink} cid
  * @param {Uint8Array} bytes
- * @returns {Promise<Uint8Array>}
+ * @returns {Promise<void>}
  */
-async function cacheDagPbBytes (env, cid, bytes) {
+async function cacheDagPbBytes(env, cid, bytes) {
   if (env.FF_DAGPB_CONTENT_CACHE_MAX_SIZE_MB && bytes.length <= env.FF_DAGPB_CONTENT_CACHE_MAX_SIZE_MB * 1024 * 1024) {
     try {
-      await env.DAGPB_CONTENT_CACHE.put(cid.multihash.digest.toString(), bytes.buffer, {
-        expirationTtl: env.FF_DAGPB_CONTENT_CACHE_TTL_SECONDS > 60 ? env.FF_DAGPB_CONTENT_CACHE_TTL_SECONDS : undefined
+      const ttlSeconds = env.FF_DAGPB_CONTENT_CACHE_TTL_SECONDS ?? 0
+      await env.DAGPB_CONTENT_CACHE.put(getDagPbKey(cid), bytes.buffer, {
+        expirationTtl: ttlSeconds > 60 ? ttlSeconds : undefined
       })
     } catch (/** @type {any} */ error) {
-      // TODO retry with exponential backoff?
-      if (error.message.includes('KV PUT failed: 429 Too Many Requests')) {
-        return bytes
-      }
       console.error(error)
     }
   }
-  return bytes
 }
 
 /**
@@ -112,10 +115,20 @@ async function cacheDagPbBytes (env, cid, bytes) {
  * @param {import('multiformats').UnknownLink} cid
  * @returns {Promise<Uint8Array | null>}
  */
-async function getCachedDagPbBytes (env, cid) {
-  const dagPbBytes = await env.DAGPB_CONTENT_CACHE.get(cid.multihash.digest.toString(), 'arrayBuffer')
+async function getCachedDagPbBytes(env, cid) {
+  const dagPbBytes = await env.DAGPB_CONTENT_CACHE.get(getDagPbKey(cid), 'arrayBuffer')
   if (dagPbBytes) {
     return new Uint8Array(dagPbBytes)
   }
   return null
+}
+
+/**
+ * Returns the base58 encoded key for the DAG Protobuf content in the KV store.
+ *
+ * @param {import('multiformats').UnknownLink} cid
+ * @returns {string}
+ */
+function getDagPbKey(cid) {
+  return base58.encode(cid.multihash.bytes)
 }
--- src/middleware/withContentClaimsDagula.types.ts
+++ src/middleware/withContentClaimsDagula.types.ts
@@ -11,12 +11,12 @@ export interface Environment extends MiddlewareEnvironment {
    * The number that represents when to expire the key-value pair in seconds from now.
    * The minimum value is 60 seconds. Any value less than 60MB will not be used.
    */
-  FF_DAGPB_CONTENT_CACHE_TTL_SECONDS: number
+  FF_DAGPB_CONTENT_CACHE_TTL_SECONDS?: number
   /**
    * The maximum size of the key-value pair in MB.
    * The minimum value is 1 MB. Any value less than 1MB will not be used.
    */
-  FF_DAGPB_CONTENT_CACHE_MAX_SIZE_MB: number
+  FF_DAGPB_CONTENT_CACHE_MAX_SIZE_MB?: number
   /**
    * The flag that enables the DAGPB content cache.
    */
